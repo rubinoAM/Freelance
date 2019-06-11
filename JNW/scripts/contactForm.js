7fde247ddba42f633cbee22fe99231cff284a86f
Michael Rubino <parkcathedralgrace@gmail.com>
2019-06-10 22:50:29 -0400

    contactForm()

--- JNW/scripts/contactForm.js
+++ JNW/scripts/contactForm.js
@@ -1,6 +1,12 @@
 function contactForm(){
-    $('#contactForm').click((e)=>{
+    $('#contactForm').submit((e)=>{
         e.preventDefault();
-        console.log("BARK");
+        
+        //Email Variables
+        let subject = e.currentTarget[0].value;
+        let name = e.currentTarget[1].value;
+        let emailAddress = e.currentTarget[2].value;
+        let emailBody = e.currentTarget[3].value;
+        console.log(subject,name,emailAddress,emailBody);
     });
 }
