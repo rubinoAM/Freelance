1ddc0e4f391d9562cfffbae5f94cf03197ee99ff
Michael Rubino <parkcathedralgrace@gmail.com>
2019-06-10 23:04:22 -0400

    contactForm()

--- JNW/scripts/contactForm.js
+++ JNW/scripts/contactForm.js
@@ -1,8 +1,10 @@
 function contactForm(){
+    const jnwEmail = 'rubinoamdeveloper@gmail.com';
+
     $('#contactForm').submit((e)=>{
         e.preventDefault();
         
-        //Email Variables
+        //User Email Variables
         let subject = e.currentTarget[0].value;
         let name = e.currentTarget[1].value;
         let emailAddress = e.currentTarget[2].value;
